01b78e16d9055c5356cdebbdf7dc353c2b0fdc55
TDW1302 <tdw1302@gmail.com>
2023-08-26 21:05:07 +0200

    First working version

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -29,57 +29,48 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(Names.AGED_BRIE.getValue())
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(Names.SULFURAS.getValue())) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals(Names.SULFURAS.getValue())) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(Names.AGED_BRIE.getValue())) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(Names.SULFURAS.getValue())) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = 0;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        for (Item item : items) {
+            String itemName = item.name;
+            if (legendaries.contains(itemName)) {
+                //Nothing to do because nothing changes for legendary stuff
+            } else if (aged.contains(itemName)) {
+                handleAged(item);
+            } else if (backstage.contains(itemName)) {
+                handleBackstage(item);
+            } else if (conjured.contains(itemName)) {
+                handleConjured(item);
+            } else { // regular stuff
+                handleRegular(item);
             }
         }
     }
 
+    private void handleRegular(Item item) {
+        item.sellIn--;
+        item.quality = Math.max(0, item.sellIn < 0 ?  item.quality-2 : item.quality-1);
+    }
 
+    private void handleConjured(Item item) {
+        item.sellIn--;
+        item.quality = Math.max(0, (item.sellIn < 0 ?  item.quality-4 : item.quality-2));
+
+    }
+
+    private void handleBackstage(Item item) {
+        item.sellIn--;
+        if(item.sellIn < 0 ){
+            item.quality = 0;
+        } else if (item.sellIn < 5 ){
+            item.quality = Math.min(50, item.quality+3);
+        } else if (item.sellIn < 10 ){
+            item.quality = Math.min(50, item.quality+2);
+        } else {
+            item.quality++;
+        }
+    }
+
+    private void handleAged(Item item) {
+        item.sellIn--;
+        item.quality = Math.min(50, item.quality+1);
+    }
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,7 +8,7 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 5, 20) };
+        Item[] items = new Item[]{new Item("foo", 5, 20)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
@@ -18,21 +18,25 @@ class GildedRoseTest {
 
     @Test
     void fooLastDay() {
-        Item[] items = new Item[] { new Item("foo", 0, 20) };
+        Item[] items = new Item[]{new Item("foo", 1, 20)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(19, app.items[0].quality);
+
+        app.updateQuality();
+        assertEquals("foo", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
-        //Currently, the quality is decreased twice : 1 as usual, and 1 because sellIn < 0
-        assertEquals(18, app.items[0].quality);
+        assertEquals(17, app.items[0].quality);
     }
 
     @Test
     void fooQualityNeverNegative() {
-        Item[] items = new Item[] { new Item("foo", 20, 5) };
+        Item[] items = new Item[]{new Item("foo", 20, 5)};
         GildedRose app = new GildedRose(items);
 
-        for (int i = 0; i < 15; i++){
+        for (int i = 0; i < 15; i++) {
             app.updateQuality();
         }
         assertEquals("foo", app.items[0].name);
@@ -43,17 +47,9 @@ class GildedRoseTest {
     @Test
     void testSulfuras() {
         String sulfurasName = Names.SULFURAS.getValue();
-        Item[] items = new Item[] { new Item(sulfurasName, 100, 80) };
+        Item[] items = new Item[]{new Item(sulfurasName, 100, 80)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(sulfurasName, app.items[0].name);
-        assertEquals(80, app.items[0].quality);
-        assertEquals(100, app.items[0].sellIn);
-
-        for (int i = 0; i <= 50; i++){
-            app.updateQuality();
-        }
-
         //Sulfuras should never change as it is legendary
         assertEquals(sulfurasName, app.items[0].name);
         assertEquals(80, app.items[0].quality);
@@ -64,14 +60,14 @@ class GildedRoseTest {
     @Test
     void testAgedBrie() {
         String brieName = Names.AGED_BRIE.getValue();
-        Item[] items = new Item[] { new Item(brieName, 15, 40) };
+        Item[] items = new Item[]{new Item(brieName, 15, 40)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(brieName, app.items[0].name);
         assertEquals(41, app.items[0].quality);
         assertEquals(14, app.items[0].sellIn);
 
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             app.updateQuality();
         }
 
@@ -84,7 +80,7 @@ class GildedRoseTest {
     @Test
     void testBackstage() {
         String backstageName = "Backstage passes to a TAFKAL80ETC concert";
-        Item[] items = new Item[] { new Item(backstageName, 11, 35) };
+        Item[] items = new Item[]{new Item(backstageName, 11, 35)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         int currentQuality = 36;
@@ -94,19 +90,19 @@ class GildedRoseTest {
         assertEquals(currentSellIn, app.items[0].sellIn);
 
 
-        for (int i = 0; i <= 4; i++){
+        for (int i = 0; i <= 4; i++) {
             app.updateQuality();
             currentQuality += 2;
-            currentSellIn -=1;
+            currentSellIn -= 1;
             assertEquals(currentQuality, app.items[0].quality);
             assertEquals(currentSellIn, app.items[0].sellIn);
         }
 
-        for (int i = 0; i <= 4; i++){
+        for (int i = 0; i <= 4; i++) {
             app.updateQuality();
             currentQuality += 3;
-            currentSellIn -=1;
-            currentQuality = Math.min(currentQuality,50);
+            currentSellIn -= 1;
+            currentQuality = Math.min(currentQuality, 50);
             assertEquals(currentQuality, app.items[0].quality);
             assertEquals(currentSellIn, app.items[0].sellIn);
         }
@@ -118,5 +114,59 @@ class GildedRoseTest {
         //Quality should be 0 after the concert
         assertEquals(0, app.items[0].quality);
         assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void testBackstageAfterConcert() {
+        String backstageName = "Backstage passes to a TAFKAL80ETC concert";
+        Item[] items = new Item[]{new Item(backstageName, 1, 35)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        int currentQuality = 38;
+        int currentSellIn = 0;
+        assertEquals(backstageName, app.items[0].name);
+        assertEquals(currentQuality, app.items[0].quality);
+        assertEquals(currentSellIn, app.items[0].sellIn);
+
+
+        app.updateQuality();
+        currentQuality = 0;
+        currentSellIn = -1;
+        assertEquals(currentQuality, app.items[0].quality);
+        assertEquals(currentSellIn, app.items[0].sellIn);
+    }
+
+
+    @Test
+    void ConjuredFoo() {
+        Item[] items = new Item[]{new Item("conjuredFoo", 5, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("conjuredFoo", app.items[0].name);
+        assertEquals(18, app.items[0].quality);
+        assertEquals(4, app.items[0].sellIn);
+    }
+
+    @Test
+    void conjuredFooLastDay() {
+        Item[] items = new Item[]{new Item("conjuredFoo", 0, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("conjuredFoo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(16, app.items[0].quality);
+    }
+
+    @Test
+    void conjuredFooQualityNeverNegative() {
+        Item[] items = new Item[]{new Item("conjuredFoo", 20, 5)};
+        GildedRose app = new GildedRose(items);
+
+        for (int i = 0; i < 15; i++) {
+            app.updateQuality();
+        }
+        assertEquals("conjuredFoo", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
+        assertEquals(5, app.items[0].sellIn);
     }
 }
